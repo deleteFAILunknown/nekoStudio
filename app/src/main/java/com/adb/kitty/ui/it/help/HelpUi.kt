e46318c54e0a7e3c0d054691fed6aba5d7d3b838
neko <neko@github.com-all>
2026-09-07 23:07:32 +0800

    Update

--- app/src/main/java/com/adb/kitty/ui/it/help/HelpUi.kt
+++ app/src/main/java/com/adb/kitty/ui/it/help/HelpUi.kt
@@ -334,18 +334,14 @@ private fun SupportAndDependencySection() {
                 )
             }
 
-            InfoRow(label = "Android Support", value = "Android 7.0 - Android 17")
-            InfoRow(label = "SDK Support", value = "24 - 37")
-            InfoRow(label = "Android TV Support", value = "Yes")
-            InfoRow(label = "abi Support", value = "arm64-v8a, armeabi-v7a, x86, x86_64, riscv64")
-            InfoRow(label = "签名方案", value = "V2 + V3 + V3.1 + V3.2 + V4(扩展签名)")
-            InfoRow(label = "签名算法", value = "SHA384 + EC-384 + EC-512 + ML-DSA-87")
-            InfoRow(label = "签名校验", value = "已启用")
-            InfoRow(label = "implementation", value = "Jetpack Compose, androidx, kotlinx, Material3, OpenSSL, Kadb, HiddenApiBypass, libsu")
-            InfoRow(label = "备案情况", value = "GitHub个人开发项目, 开发者拒绝备案, 如介意, 可随时卸载, 开发者不会剥夺用户的使用权")
-            InfoRow(label = "云端接入情况", value = "APP不接入任何云端以及任何会泄露隐私的远程API, 如果您安装的APP接入了云端以及远程API, 那么您使用的APP极有可能是来自三方编译或来自GitHub的其他叉子编译构建的版本")
-            InfoRow(label = "GitHub", value = "https://github.com/deleteFAILunknown/nekoStudio")
-            InfoRow(label = "正在使用的外部存储目录", value = "/storage/emulated/0/Android/data/com.adb.kitty/files/flash/")
+            InfoRow(label = "System", value = "Android 7.0 - 17")
+            InfoRow(label = "api", value = "24 - 37")
+            InfoRow(label = "abi", value = "arm64-v8a, armeabi-v7a, x86, x86_64, riscv64")
+            InfoRow(label = "github", value = "https://github.com/deleteFAILunknown/nekoStudio")
+            InfoRow(label = "fastboot数据目录", value = "/storage/emulated/0/Android/data/com.adb.kitty/files/flash")
+            InfoRow(label = "adb数据目录", value = "/storage/emulated/0/Android/data/com.adb.kitty/files/flash")
+            InfoRow(label = "cpu数据目录", value = "/storage/emulated/0/Android/data/com.adb.kitty/files/cpu")
+            InfoRow(label = "log数据目录", value = "/storage/emulated/0/Android/data/com.adb.kitty/cache/logs")
         }
     }
 }
