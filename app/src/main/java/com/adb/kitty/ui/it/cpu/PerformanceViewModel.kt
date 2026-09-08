7ef8d52ce73f8d266e80a626a929b17f83a86a9d
neko <neko@github.com-all>
2026-09-08 14:35:49 +0800

    Update

--- app/src/main/java/com/adb/kitty/ui/it/cpu/PerformanceViewModel.kt
+++ app/src/main/java/com/adb/kitty/ui/it/cpu/PerformanceViewModel.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.Job
 import java.text.SimpleDateFormat
