5a2632c7033a0978f25480ecc8b158fae2bc4198
neko <neko@github.com-all>
2026-09-10 11:53:52 +0800

    Update

--- app/src/main/java/com/adb/kitty/ui/it/cpu/HistoryPerformanceBottomSheet.kt
+++ app/src/main/java/com/adb/kitty/ui/it/cpu/HistoryPerformanceBottomSheet.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
