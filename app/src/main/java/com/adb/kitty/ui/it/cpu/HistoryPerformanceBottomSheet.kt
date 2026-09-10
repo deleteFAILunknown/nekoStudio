13a01e0b30cdff12c8d752a96260414b814cae54
neko <neko@github.com-all>
2026-09-10 11:34:55 +0800

    Update

--- app/src/main/java/com/adb/kitty/ui/it/cpu/HistoryPerformanceBottomSheet.kt
+++ app/src/main/java/com/adb/kitty/ui/it/cpu/HistoryPerformanceBottomSheet.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
