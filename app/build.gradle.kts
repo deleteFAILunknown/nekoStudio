f06dc6cb1e22c5303b777188ef3135c217ccf7f6
neko <neko@github.com-all>
2026-09-16 03:04:08 +0800

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -134,8 +134,6 @@ android {
             optimization.keepRules {
                 // ignoreFrom 只允许忽略来自远程库的依赖
                 ignoreFrom("org.jetbrains.kotlinx:kotlinx-coroutines-android")
-                ignoreFrom("org.jetbrains.kotlinx:kotlinx-serialization-core")
-                ignoreFrom("org.jetbrains.kotlinx:kotlinx-serialization-json")
                 ignoreFrom("org.lsposed.hiddenapibypass:hiddenapibypass")
                 ignoreFrom("com.github.topjohnwu.libsu:core")
             }
