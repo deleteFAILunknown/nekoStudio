fef35da96c0f4f11084f4fe62744cc7af1e00147
neko <neko@github.com-all>
2026-09-19 08:33:01 +0800

    Update

--- libs/src/main/java/libs/libs/libs/adb/services/AdbServices.kt
+++
@@ -1,8 +0,0 @@
-package libs.libs.libs.adb.services
-
-import libs.libs.libs.adb.session.AdbConnection
-
-public class AdbServices(public val connection: AdbConnection) {
-    public val shell: AdbShellService = AdbShellService(connection)
-    public val sync: AdbSyncService = AdbSyncService(connection)
-}
