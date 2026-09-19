c0090cb2231988992904023f6efd50facce1a9ed
neko <neko@github.com-all>
2026-09-19 09:19:38 +0800

    Update

--- libs/src/main/java/libs/libs/libs/adb/pairing/AdbPairingClient.kt
+++ libs/src/main/java/libs/libs/libs/adb/pairing/AdbPairingClient.kt
@@ -13,8 +13,10 @@ public class AdbPairingClient(
     public val crypto: AdbCrypto
 ) {
 
-    public const var PAIRING_REQ_TYPE: Byte = 1
-    public const var PAIRING_RESP_TYPE: Byte = 2
+    public companion object {
+        public const val PAIRING_REQ_TYPE: Byte = 1
+        public const val PAIRING_RESP_TYPE: Byte = 2
+    }
 
     public suspend fun pair(pairingCode: String, timeoutMs: Int = 10000): Boolean = withContext(Dispatchers.IO) {
         val transport = TlsTransport(host, port, crypto, timeoutMs)
