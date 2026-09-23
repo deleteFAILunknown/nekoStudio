6dd79fbba9559b98d11f70b4c1f3a608596b60e2
neko <neko@github.com-all>
2026-09-23 23:58:00 +0800

    Update

--- libs/src/main/java/libs/libs/libs/adb/sync/FilePermissions.kt
+++ libs/src/main/java/libs/libs/libs/adb/sync/FilePermissions.kt
@@ -1,9 +1,13 @@
 package libs.libs.libs.adb.sync
 
 /**
- * 默认的文件权限模式 (Linux 0644 S_IFREG)
+ * 默认的文件权限模式
  */
 public object FilePermissions {
-    public const val S_IFREG: Int = 0100000 // 普通文件
-    public const val DEFAULT_MODE: Int = S_IFREG or 0644
+    // 八进制 0100000 (S_IFREG 普通文件) 对应十六进制 0x8000
+    public const val S_IFREG: Int = 0x8000
+
+    // 八进制 0644 (rw-r--r--) 对应十六进制 0x01A4 (420)
+    // 八进制 0755 (rwxr-xr-x) 对应十六进制 0x01ED (493)
+    public const val DEFAULT_MODE: Int = S_IFREG or 0x01A4
 }
