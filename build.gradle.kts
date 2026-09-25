5ca8e402b50c67b8f30cf23331d5234273abf147
neko <neko@github.com-all>
2026-09-25 09:49:06 +0800

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,17 +9,4 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
-val specialJar = tasks.register<Jar>("specialJar") {
-    archiveBaseName.set("special")
-    from("build/special")
-}
-
-configurations {
-    consumable("special") {
-        outgoing.artifact(specialJar)
-    }
-}
-
-tasks.named("assemble") {
-    dependsOn(specialJar)
-}
+tasks.named("assemble") {}
